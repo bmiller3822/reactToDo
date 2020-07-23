ea2203f17b14ae3fa5f7ec1d1cb395e7c28c04bc
Brandon <bmiller3822@gmail.com>
2020-07-23 16:08:16 -0400

    Built todoitemjs

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import { render } from '@testing-library/react';
 
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with the wife",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Go grocery shopping",
+        completed: true
+      }
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Learn React</a>
-        </header>
+        <Todos todos={this.state.todos} />
       </div>
     )
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 
-function Todos() {
-    return (
-        <div>
-            <h1>Todos</h1>
-        </div>
-    );
+
+class Todos extends Component {
+    render() {
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} />
+        ));
+    }
 }
+
 
 export default Todos;
